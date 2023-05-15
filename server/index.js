c95f4a249d3e01ca0f82efb14a7c34d9fb9f27d4
Dmitry Evsey <cfgkk.evsey@gmail.com>
2023-05-15 21:02:10 +0300

    started server and connected db

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+const PORT = process.env.PORT || 7000;
+
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        app.listen(PORT, () => console.log(`Сервер был запущен на ${PORT} порту`));
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start();
