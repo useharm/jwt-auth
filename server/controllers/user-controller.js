b9af8c4f63acbb978656dc39181b20480ffa0f8d
Dmitry Evsey <cfgkk.evsey@gmail.com>
2023-05-15 21:34:24 +0300

    created routes and controllers

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,50 @@
+
+
+
+
+class UserController {
+    async registration(req, res, next) {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    async login(req, res, next) {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    async logout(req, res, next) {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    async refresh(req, res, next) {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    async activate(req, res, next) {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    async getUsers(req, res, next) {
+        try {
+            return res.json(['hello'])
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+module.exports = new UserController();
