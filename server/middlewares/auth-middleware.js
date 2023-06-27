f2501d2baaab66e9ca8177a871d3de3dc7679187
useharm <cfgkk.evsey@gmail.com>
2023-06-27 21:14:08 +0300

    fixes for error-middleware

--- server/middlewares/auth-middleware.js
+++ server/middlewares/auth-middleware.js
@@ -1,18 +1,19 @@
 const tokenService = require("../services/token-service");
+const ApiError = require('../exceptions/api-errors');
 
 module.exports = function AuthMiddleware(req, res, next) {
     try {
         const authData = req.headers.authorization;
         if (!authData) {
-            throw new Error('Ошибка авторизации');
+            throw ApiError.UnauthorizedError();
         }
         const accessToken = authData.split(' ')[1];
         if (!accessToken) {
-            throw new Error('Ошибка авторизации');
+            throw ApiError.UnauthorizedError();
         }
         const user = tokenService.validateAccess(accessToken);
         if (!user) {
-            throw new Error('Ошибка авторизации');
+            throw ApiError.UnauthorizedError();
         }
         req.user = user; 
         next();
